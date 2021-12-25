f5bf180f6a76cd0b738b21b3363187de2b16b92d
Andre gois <andre.s.gois3@gmail.com>
2021-12-25 10:36:58 -0300

    authentication via firebase

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import {  StyleSheet,  Text,  TouchableOpacity,  View} from 'react-native';
+import {  Button, FlatList, StyleSheet,  Text,  TouchableOpacity,  View} from 'react-native';
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set, remove, update, push, child } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getDatabase, ref, set, remove, update, push, child, onValue } from "firebase/database";
 import { APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID, MEASUREMENTID} from '@env';
 
 class App extends Component {
 
-  //UNSAFE_componentWillMount(){
-    componentDidMount(){
+  
+  constructor(props){
+    super(props)
     const firebaseConfig = {
       apiKey: APIKEY,
       authDomain: AUTHDOMAIN,
@@ -20,20 +22,59 @@ class App extends Component {
       appId: APPID,
       measurementId: MEASUREMENTID
     };
-
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const analytics = getAnalytics(app);
+    const db = getDatabase();
+    this.state = { users: [] }
+  }
+
+  cadastrarUser(){
+    console.log("aqui")
+    const email = "andre.s.gois3@gmail";
+    const password = "5101621apb*";
+    const auth = getAuth();
+    console.log(auth);
+
+    // não funciona
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        console.log(userCredential)
+        const user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+  
+  componentWillMount(){ 
+    const starCountRef = ref(getDatabase(), 'users');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      //console.log(data)
+      //console.log(typeof data)
+      this.setState({ users: data})      
+      //console.log(this.state.users)
+      //updateStarCount(postElement, data);
+    });
   }
 
   saveData(){
     console.log('Save data')
+    this.writeUserData("001", "André", "andre@email.com", 18)
+    this.writeUserData("002", "Priscila", "pri@email.com", 22)
+    this.writeUserData("003", "Luiza", "luiza@email.com", 25)
     // var database = app.database();
     // database.ref("pontuacao").set("100")
-    const database = getDatabase();
+    /*const database = getDatabase();
     set(ref(database, 'Pontuacao/'), {
-      pontos: 200
-    })
+      pontos: 600
+    })*/
     //remove(ref(database, 'Pontuacao/'))
     //update(ref("pontuacao"), '1589' )
 
@@ -41,10 +82,35 @@ class App extends Component {
     // const updates = {};
     // updates['pontos'] = 1569;
     // update(ref(database, 'Pontuacao'), updates)
-    // console.log(updates);
+    // console.log(updates);    
+  }
+
+  writeUserData(userId, name, email, age) {    
+    set(ref(getDatabase(), 'users/'+ userId), {
+      id: userId,
+      username: name,
+      email: email,
+      age : age
+    });
+  }
+
+  handleClick(v){
+    console.log(v)
+  }
+
+  handleDelete(v){
+    console.log(v.id)
+    const p = remove(ref(getDatabase(), 'users/'+v.id))
+    console.log(p)
+    //remove(ref(getDatabase(), 'users/'))
   }
   
+  
   render(){
+    let { users } = this.state;
+    users !== null ? users = Object.values(users): null;
+    //console.log(users)
+ 
     return (
       <View style={styles.container}>
         <TouchableOpacity
@@ -53,7 +119,26 @@ class App extends Component {
         >
           <Text style={styles.text}>Salvar dados</Text>
         </TouchableOpacity>
-        <Text>My App</Text>
+        <Text style={{ textAlign: 'center', fontSize: 28, fontWeight: '800'}}>Usuários</Text>
+        <Button           
+          onPress={ () => this.cadastrarUser() }
+          title='Cadastrar User'
+        />
+        
+        <View>
+          { users !== null ?
+            users.map( (user) => (
+              <View key={user.email} style={{ flexDirection: 'row'}}>
+                <Text style={{ padding: 2, margin: 5}}>{user.username}</Text> 
+                <Text style={{ padding: 2, margin: 5}}>{user.email}</Text> 
+                <Text style={{ padding: 2, margin: 5}}>{user.age}</Text>
+                <Button onPress={ () => this.handleClick(user)} title='Editar' />
+                <Button onPress={ () => this.handleDelete(user)} title='Deletar' />
+              </View>
+            ))
+            : <Text>Sem Informações</Text>
+          }
+        </View>
       </View>
     );
   }
